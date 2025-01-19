592c3bc94e49fe5bcaab5b92318ac86096cc7454
Kieran Coughlan <KieranCoughlan@gmail.com>
2025-01-19 16:53:44 +0000

    Minor tweak

--- Crystal Quest/crystal-quest/scripts/mouse_hider.gd
+++ Crystal Quest/crystal-quest/scripts/mouse_hider.gd
@@ -1,10 +1,5 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var vp_rect : Rect2 = get_viewport().get_visible_rect()
