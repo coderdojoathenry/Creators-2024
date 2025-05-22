68c23502317455287c07b8545eaea29baf45a0af
Kieran Coughlan <KieranCoughlan@gmail.com>
2025-05-23 00:19:51 +0100

    Levels

--- Crystal Quest/crystal-quest/scripts/game_manager.gd
+++ Crystal Quest/crystal-quest/scripts/game_manager.gd
@@ -7,6 +7,8 @@ class_name GameManager extends Node2D
 @export var explosion_effect : PackedScene
 @export var respawn_delay : float = 3.0
 @export var life_icons : Array[Control]
+@export var levels : Array[PackedScene]
+@export var bonus_speed_label: Label
 
 @onready var spawn_layer: Node2D = %SpawnLayer
 @onready var respawn_timer: Timer = $RespawnTimer
@@ -14,12 +16,16 @@ class_name GameManager extends Node2D
 @onready var collect_sound: AudioStreamPlayer2D = $CollectSound
 @onready var base_left : Node2D = $"../Environment/Base_left"
 @onready var base_right : Node2D = $"../Environment/Base_right"
+@onready var next_level_timer: Timer = $NextLevelTimer
+@onready var success_sound: AudioStreamPlayer2D = $SuccessSound
 
 var ship_initial_position : Vector2
 var explosion_instance : Node2D
 var max_lives : int = 5
 var lives : int = 3
 var ship_destroyed : bool = false
+var level_number : int = -1
+var current_level : Level
 
 func _ready() -> void:
 	GlobalObjects.GameManager = self
@@ -35,6 +41,8 @@ func _ready() -> void:
 	if (explosion_effect):
 		explosion_instance = explosion_effect.instantiate() as Node2D
 		add_child(explosion_instance)
+	
+	load_next_level()
 
 func _process(delta: float) -> void:
 	if (score_label):
@@ -84,4 +92,27 @@ func _on_level_cleared() -> void:
 	print_debug("Level cleared")
 
 func level_exited() -> void:
-	print_debug("Level exited")
+	get_parent().remove_child(ship)
+	ship_destroyed = true
+	if (current_level.bonus_time > 0.0):
+		increase_score(50000)
+		bonus_speed_label.show()
+	next_level_timer.start(4)
+	GlobalObjects.reset_play_area.emit()
+	success_sound.play()
+	
+func load_next_level() -> void:
+	if (current_level != null):
+		current_level.queue_free()
+	level_number += 1
+	var effective_level = level_number % levels.size()
+	current_level = levels[effective_level].instantiate() as Level
+	add_child(current_level)
+
+
+func _on_next_level_timer_timeout() -> void:
+	bonus_speed_label.hide()
+	load_next_level()
+	ship.global_position = ship_initial_position
+	get_parent().add_child(ship)
+	ship_destroyed = false
--- Crystal Quest/crystal-quest/scripts/level.gd
+++ Crystal Quest/crystal-quest/scripts/level.gd
@@ -1,16 +1,17 @@
-extends Node2D
+class_name Level extends Node2D
+
+@export var bonus_time : float = 4.0
 
 var level_cleared : bool = false
 @onready var crystals = $Crystals
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	bonus_time -= delta
 	if (crystals.get_child_count() == 0 && level_cleared == false):
 		level_cleared = true
 		GlobalObjects.crystals_cleared.emit()
